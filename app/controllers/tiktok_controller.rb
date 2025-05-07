a51d426e7a067b96c413a40257e459b4ee4dd010
Victor <viktor.markevich@faceit.com.ua>
2025-05-07 10:00:40 +0200

    Fix turbo striming

--- app/controllers/tiktok_controller.rb
+++ app/controllers/tiktok_controller.rb
@@ -1,26 +1,13 @@
 class TiktokController < ApplicationController
-  # def campaigns
-  #   advertiser_id = ENV["TIKTOK_ADVERTISER_ID"]
-  #   client = TiktokBusinessApi.client(access_token: ENV["TIKTOK_ACCESS_TOKEN"])
-  #   @campaigns = client.campaigns.list(advertiser_id)["data"]["list"]
-  #
-  #   render partial: "contexts/campaign_select", locals: { context: Context.new, campaigns: @campaigns }
-  # end
-
   def adgroups
     campaign_id = params[:campaign_id]
     @context = Context.new(campaign_id: campaign_id)
     @adgroups = TiktokAdgroupsFetcher.call(campaign_id)
-    p '*'*100
-    p @adgroups
-    p '*'*100
 
     render turbo_stream: turbo_stream.replace(
       "adgroups_frame",
       partial: "contexts/adgroup_select",
       locals: { context: @context, adgroups: @adgroups }
     )
-
-    render partial: "contexts/adgroup_select", locals: { context: Context.new, adgroups: @adgroups }
   end
 end
--- app/javascript/controllers/tiktok_binding_controller.js
+++ app/javascript/controllers/tiktok_binding_controller.js
@@ -1,15 +1,16 @@
-import { Controller } from "@hotwired/stimulus"
+import {Controller} from "@hotwired/stimulus"
 
 export default class extends Controller {
     static targets = ["campaignSelect"]
 
     updateAdgroups(event) {
         const campaignId = this.campaignSelectTarget.value
-        console.log(campaignId)
+
         const url = `/tiktok/adgroups?campaign_id=${campaignId}`
 
         fetch(url, {
-            headers: { "Accept": "text/vnd.turbo-stream.html" }
-        })
+            headers: {"Accept": "text/vnd.turbo-stream.html"}
+        }).then(response => response.text())
+            .then(html => Turbo.renderStreamMessage(html))
     }
 }
