f22257ad0f8617f59c1b234e176837733dfc1610
Victor <viktor.markevich@faceit.com.ua>
2025-05-07 10:32:10 +0200

    Generate ideas again

--- app/helpers/contexts_helper.rb
+++ app/helpers/contexts_helper.rb
@@ -1,7 +1,7 @@
 module ContextsHelper
   def render_image_generation_cta(context)
     if context.ad_ideas.any?
-      button_name = context.generated_images.any? ? 'Generate Images Again' : 'Generate images'
+      button_name = context.generated_images.any? ? 'Generate images again' : 'Generate images'
       button_to button_name, generate_images_context_path(context), method: :post
     else
       raw("<span style='color: orange'>Please, generate ideas first</span>")
